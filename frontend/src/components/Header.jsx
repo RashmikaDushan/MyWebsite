577de50574f877182c83386b391470ed9cba66d8
Rashmika Dushan <kbrashmikadushan@gmail.com>
2023-11-30 00:53:05 +0530

    added profile pic

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import "./Header.css";
+import profile from "../assets/Dushan_square.jpg";
 
 class Header extends Component {
   render() {
     return (
       <div className="head">
-        <div className="caption">Hola...</div>
-        <p>
-          I'm Rashmika Dushan. Highly passionated Computer Science and
-          Engineering undergraduate from University of Moratuwa.
-        </p>
+        <div className="container">
+          <div className="texts">
+            <div className="caption">Hola...</div>
+            <p>
+              I'm Rashmika Dushan. Highly passionated Computer Science and
+              Engineering undergraduate from University of Moratuwa.
+            </p>
+          </div>
+          <img src={profile} className="profile" />
+        </div>
       </div>
     );
   }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,19 @@ import "./Navbar.css";
 
 class Navbar extends Component {
   render() {
-    return <div className="nav">Navbar</div>;
+    return (
+      <div className="nav">
+        <a>
+          <span>Home</span>
+        </a>
+        <a>
+          <span>Projects</span>
+        </a>
+        <a>
+          <span>Contact</span>
+        </a>
+      </div>
+    );
   }
 }
 
